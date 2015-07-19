c8b525d6fc0eedb960bb50fac39de812ab895c66
kongvut sangkla <kongvut@gmail.com>
2015-07-19 10:03:44 +0700

    Revert "update"
    This reverts commit 0d2cde3c37cec935bc727777d3c20755a67ef36c.

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 ## System Start ############################################################
-$mysqli = new mysqli("localhost", "root", "root", "starterphp");
+$mysqli = new mysqli("localhost", "root", "pass", "db_name");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
